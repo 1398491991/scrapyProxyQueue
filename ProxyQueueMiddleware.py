d084a4023d5f205f581b3b63955ab498be37d08b
zzl <zhanzl@i2dd.com>
2016-08-17 14:46:33 +0800

    update

--- ProxyQueueMiddleware.py
+++ ProxyQueueMiddleware.py
@@ -9,7 +9,7 @@ try:
 except ImportError:
     # py3
     from queue import Queue,Empty,Full
-
+import random
 logger = logging.getLogger(__name__)
 queue=Queue(maxsize=10)  # 用于存储代理  获取代理爬虫暂时没写
 
@@ -17,23 +17,27 @@ class Proxy(object):
     def __init__(self,ip,port,user_pass=None,proxy_retry_times=0):
         self._ip=ip
         self._port=port
-        self._proxy_retry_times=proxy_retry_times
+        self._proxy_retry_times=proxy_retry_times # 使用重试的次数
         self._user_pass=user_pass
 
+    @property
     def get_ip(self):
         return self._ip
 
+    @property
     def get_port(self):
         return self._port
 
+    @property
     def get_proxy_retry_times(self):
         return self._proxy_retry_times
 
+    @property
     def join_ip_port(self):
         return '%s:%s'%(self._ip,self._port)
 
     def proxy_format(self):
-        return 'http://'+self.join_ip_port()
+        return 'http://'+self.join_ip_port
 
     def update_proxy_retry_times(self):
         self._proxy_retry_times+=1
@@ -51,26 +55,43 @@ class SetProxyMiddleware(object):
     def __init__(self, settings):
         self.queue_block=settings.getbool('QUEUE_BLOCK',default=False)
         self.queue_timeout=settings.getint('QUEUE_TIMEOUT',default=0)
+        self.standby_proxy_list=settings.getlist('STANDBY_PROXY') # 备用的代理
+        # 格式 [(ip,port,user_pass),]   没有 user_pass 则为 None
 
+        if not self.standby_proxy_list:
+            logging.warning("STANDBY_PROXY is empty , This is bad")  # 没有备用代理  无法在队列为空时 再次使用代理
+        if self.queue_block and self.queue_timeout>0.5:
+            logging.warning("QUEUE_TIMEOUT is too large , This is bad")  # 代理队列等待时间过长 影响爬虫的并发效率
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler.settings)
 
     def process_request(self, request, spider):
-        # Set the location of the proxy
+        # 给请求设置代理
         try:
             proxy = queue.get(block=self.queue_block,timeout=self.queue_timeout)
-            request.meta['proxy'] = proxy.proxy_format()
-            request.meta['proxy_instantiation']=proxy
-            request.headers['Proxy-Authorization'] = proxy.proxy_authorization()
-            logging.debug("User Proxy : %s"%proxy)
+            self._set_proxy(request,proxy)
 
         except Empty:
-            logging.warning("Proxy Queue is empty")
+            logging.warning("Proxy Queue is empty")  # 代理队列为空的
+            try:
+                ip,port,user_pass=random.choice(self.standby_proxy_list)
+                user_pass=user_pass or None
+                proxy=Proxy(ip,port,user_pass)
+                self._set_proxy(request,proxy)
+
+            except ValueError:
+                logging.warning("STANDBY_PROXY formal error . example : [(ip,port,user_pass), (ip,port,user_pass)]")
 
         except Exception as e:
-            logging.error("Unknown Error : %s"%e)
+            logging.error("Unknown Error : %s"%e)   # 产生未知的错误
+
+    def _set_proxy(self,request,proxy):
+        request.meta['proxy'] = proxy.proxy_format()
+        request.meta['proxy_instantiation']=proxy # 用于存储代理的实例  后期会有 pick_up_proxy 操作
+        request.headers['Proxy-Authorization'] = proxy.proxy_authorization()
+        logging.debug("User Proxy : %s"%proxy)
 
 class ProxyQueueRetryMiddleware(RetryMiddleware):
     # EXCEPTIONS_TO_RETRY = (defer.TimeoutError, TimeoutError, DNSLookupError,
